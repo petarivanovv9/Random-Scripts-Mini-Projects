c36024d74b026a23d8913345569389e39e513f2a
Petar Ivanov <pepincho95@gmail.com>
2018-02-20 17:00:59 +0200

    Add video file extension check

--- youtube-js/upload_all_videos.sh
+++ youtube-js/upload_all_videos.sh
@@ -5,13 +5,34 @@ printf "\n=>> Start uploading all videos from $1\n"
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+# Youtube video formats
+# .MOV
+# .MPEG4
+# .MP4
+# .AVI
+# .WMV
+# .MPEGPS
+# .FLV
+# 3GPP
+# WebM
+# DNxHR
+# ProRes
+# CineForm
+# HEVC (h265)
+
+
+# for video in $(find "$1"* -type f -regex '.*\.\(avi\|mkv\|mp4\|wmv\|flv\|webm\|mov\)'); do
 for video in "$1"*; do
-    printf "\nuploading --> $video\n"
 
-    node $SCRIPTPATH/upload_video.js $video
+    if [[ $video == *.avi || $video == *.mp4 || $video == *.mov || $video == *.mpeg4 || $video == *.wmv || $video == *.mpegps || $video == *.flv ]]
+    then
+      printf "\nuploading --> $video\n"
 
-    printf "\n"
+      # node $SCRIPTPATH/upload_video.js $video
+
+      printf "\n"
+    fi
 done
 
 
-printf "End of uploading\n"
+printf "\nEnd of uploading\n"
