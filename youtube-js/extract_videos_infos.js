f39f060159475df11b5de1cc914898a103166e89
Petar Ivanov <pepincho95@gmail.com>
2018-03-01 10:36:43 +0200

    Add need video info extraction

--- youtube-js/extract_videos_infos.js
+++ youtube-js/extract_videos_infos.js
@@ -1,5 +1,37 @@
 'use strict';
 
 /**
- * Usage: node extract_video_infos.js PATH_TO_VIDEOS_INFOS PATH_TO_FILE_TO_WRITE
+ * Usage: node extract_video_infos.js PATH_TO_VIDEO_INFO_FILE PATH_TO_FILE_TO_WRITE
  */
+
+
+const fs = require('fs');
+
+
+const VIDEO_INFO_FILENAME = process.argv[2];
+const RESULT_FILENAME = process.argv[3];
+
+
+const videoInfo = getNecessaryVideoInfo(VIDEO_INFO_FILENAME);
+
+console.log(videoInfo);
+
+
+function getNecessaryVideoInfo(filename) {
+    const rawVideoInfo = JSON.parse(fs.readFileSync(filename, 'utf8'));
+
+    return {
+        "video_id": rawVideoInfo.id,
+        "video_url": rawVideoInfo.webpage_url,
+        "title": rawVideoInfo.title,
+        "description": rawVideoInfo.description,
+        "duration(human)": '-1',
+        "duration(secs)": rawVideoInfo.duration,
+        "view_count": rawVideoInfo.view_count,
+        "like_count": rawVideoInfo.like_count,
+        "dislike_count": rawVideoInfo.dislike_count,
+        "upload_date": rawVideoInfo.upload_date,
+        "uploader_id": rawVideoInfo.uploader_id,
+        "uploader_url": rawVideoInfo.uploader_url
+    };
+}
